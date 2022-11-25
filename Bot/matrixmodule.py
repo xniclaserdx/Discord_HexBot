ef6bd7f9d4b018acf6bbf63d7cb0afea629c8bc1
Tore Kunze <tore.kunze@rwth-aachen.de>
2022-11-25 12:36:33 +0100

    clean the code up in matrixmodule

--- Bot/matrixmodule.py
+++ Bot/matrixmodule.py
@@ -3,13 +3,13 @@ import numpy as np
 def mathInputEvaluate(args):
     arglen = len(args)
     if arglen <= 1:
-        return "Zu wenig input uff"
+        return "Bitte gebe weitere Argumente an"
     formattedInputList = [None]*arglen
     for i in range(arglen):
         try:
             formattedInputList[i] = formatInput(args[i])
         except:
-            return "Fehler du Trollo"
+            return "Formatierungsfehler"
     return eval(formattedInputList)
     
 def printvals(elementList):
@@ -20,6 +20,20 @@ def eval(elementList):
     if len(elementList) == 1:
         return elementList[0].value
     # bracket evaluation
+    brackEval = bracketEvaluation(elementList)
+    if brackEval != None:
+        return brackEval
+    # multiplication evaluation
+    multEval = multEvaluation(elementList)
+    if multEval != None:
+        return multEval
+    # addition and subtraction evaluation
+    addsubEval = addsubEvaluation(elementList)
+    if addsubEval.any() != None:
+        return addsubEval
+
+# bracketEvaluation     
+def bracketEvaluation(elementList):
     firstbracketIndex = -1
     secondbracketIndex = -1
     openBracketCount = 0
@@ -27,88 +41,92 @@ def eval(elementList):
     for i in range(len(elementList)):
         if elementList[i].type == "bracket" and elementList[i].value == "(":
             openBracketCount += 1
+            # find innermost opening bracket
             if openBracketCount > maxBracketCount:
                 maxBracketCount = openBracketCount
                 firstbracketIndex = i
         elif elementList[i].type == "bracket" and elementList[i].value == ")":
             openBracketCount -= 1
+            # too many closed brackets
             if openBracketCount < 0:
                 raise BracketMismatchError
+            # search for the first closing bracket after the most innermost opening one
             if secondbracketIndex < firstbracketIndex:
                 secondbracketIndex = i
+    # too many open brackets
     if openBracketCount != 0:
         raise BracketMismatchError
+    # brackets found
     if firstbracketIndex != -1:
         if firstbracketIndex + 1 == secondbracketIndex:
             raise EmptyBracketsError
         bracketEval = eval(elementList[firstbracketIndex+1:secondbracketIndex])
         beforeBrack = elementList[0:firstbracketIndex]
         afterBrack = []
+        # check if second bracket is last element
         if secondbracketIndex != len(elementList)-1:
             afterBrack = elementList[secondbracketIndex+1:len(elementList)]
         else:
             afterBrack = []
-        toEval = beforeBrack
-        toEval.append(Element(bracketEval,"matrix","None"))
-        toEval.extend(afterBrack)
-        return eval(toEval)
-    # multiplication evaluation
+        res = elementsJoin(beforeBrack,bracketEval,afterBrack)
+        return eval(res)
+    return None
+            
+def multEvaluation(elementList):
     for i in range(len(elementList)):
-        if elementList[i].type == "operator" and elementList[i].value == "*":
-            if i == 0 or i == len(elementList) or elementList[i-1].type != "matrix" or elementList[i+1].type != "matrix":
+        if elementList[i].type == "operator" and elementList[i].value == "X":
+            if not validBinaryOpPosition(elementList,i):
                 raise MultiplicationOperandError
-            beforeMult = elementList[0:i-1]
-            afterMult = []
-            if i+1 == len(elementList):
-                afterMult = []
-            else:
-                afterMult = elementList[i+2:len(elementList)]
+            beforeMult, afterMult = splitElementListForBinaryOp(elementList,i)
             try:
                 multEval = np.matmul(elementList[i-1].value,elementList[i+1].value)
             except:
                 raise MatrixMultiplicationError
-            toEval = beforeMult
-            toEval.append(Element(multEval,"matrix","None"))
-            toEval.extend(afterMult)
-            return eval(toEval)
+            res = elementsJoin(beforeMult,multEval,afterMult)
+            return eval(res)
+    return None
+
+def addsubEvaluation(elementList):
     for i in range(len(elementList)):
         if elementList[i].type == "operator" and (elementList[i].value == "+" or elementList[i].value == "-"):
-            if i == 0 or i == len(elementList) or elementList[i-1].type != "matrix" or elementList[i+1].type != "matrix":
+            if not validBinaryOpPosition(elementList,i):
                 match elementList[i].value:
                     case "+":
                         raise AdditionOperandError
                     case "-":
                         raise SubtractionOperandError
-            beforeOp = elementList[0:i-1]
-            afterOp = []
-            if i+1 == len(elementList):
-                afterOp = []
-            else:
-                afterOp = elementList[i+2:len(elementList)]
+            beforeOp, afterOp = splitElementListForBinaryOp(elementList,i)
             match elementList[i].value:
                 case "+":
-                    # hier funktioniert was
                     try:
                         addEval = elementList[i-1].value + elementList[i+1].value
                     except:
                         raise MatrixAdditionError
-                    toEval = beforeOp
-                    toEval.append(Element(addEval,"matrix","None"))
-                    toEval.extend(afterOp)
-                    return eval(toEval)
+                    res = elementsJoin(beforeOp,addEval,afterOp)
+                    return eval(res)
                 case "-":
                     try:
                         subEval = elementList[i-1].value - elementList[i+1].value
                     except:
                         raise MatrixSubtractionError
-                    toEval = beforeOp
-                    toEval.append(Element(subEval,"matrix","None"))
-                    toEval.extend(afterOp)
-                    return eval(toEval)
+                    res = elementsJoin(beforeOp,subEval,afterOp)
+                    return eval(res)
 
-            
-
-
+def validBinaryOpPosition(elementList,i):
+    if i == 0 or i+1 == len(elementList) or elementList[i-1].type != "matrix" or elementList[i+1].type != "matrix":
+        return False
+    else:
+        return True
+                
+def splitElementListForBinaryOp(elementList,i):
+    beforeOp = elementList[0:i-1]
+    afterOp = []
+    # check if there are elements left to the right of the binary operation
+    if i+2 == len(elementList):
+        afterOp = []
+    else:
+        afterOp = elementList[i+2:len(elementList)]
+    return beforeOp, afterOp
 
 def toMatrix(string):
     matrStr = textMatrixFormat(string)
@@ -116,6 +134,13 @@ def toMatrix(string):
         return np.matrix(matrStr)
     except:
         raise MatrixParseError
+    
+def elementsJoin(list1,matrix,list2):
+    res = list1
+    res.append(Element(matrix,"Matrix","None"))
+    res.extend(list2)
+    return res
+
      
 def formatInput(input):
     match input:
@@ -123,18 +148,18 @@ def formatInput(input):
             return Element("+","operator","None")
         case "-":
             return Element("-","operator","None")
-        case "*":
-            return Element("*","operator","None")
+        case "X":
+            return Element("X","operator","None")
         case "(":
             return Element("(","bracket","None")
         case ")":
             return Element(")","bracket","None")
         case _:
             matString = textMatrixFormat(input)
-            #try: 
-            return Element(np.matrix(matString),"matrix","None")
-            #except:
-            #    raise MatrixParseError
+            try: 
+                return Element(np.matrix(matString),"matrix","None")
+            except:
+                raise MatrixParseError
 
 # changes user input to allow conversion into a matrix
 def textMatrixFormat(input):
