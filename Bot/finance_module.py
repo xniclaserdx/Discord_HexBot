513156fe1b356e36d766e1751df1f7efb0c70934
Niclas <61380272+xniclaserdx@users.noreply.github.com>
2022-12-29 23:14:57 +0100

    get_history update, plot for tickers
    Implement plotting for stock prices of the last year.

--- Bot/finance_module.py
+++ Bot/finance_module.py
@@ -1,6 +1,11 @@
 import yfinance as yahooFinance
 import datetime
 import interactions
+from interactions.ext.files import command_send
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import io
+import os 
 
 def get_market_price(ticker):
     try:
@@ -12,33 +17,59 @@ def get_market_price(ticker):
     except:
         return f"No data found."
 def get_history(ticker):
-    start_date = "2020-01-01"
-    end_date = "2022-01-01"
-
-    # Get the data
+    now = datetime.datetime.now()
+    start_date = (now - datetime.timedelta(days=365*2)).strftime("%Y-%m-%d")
+    end_date = (now + datetime.timedelta(days=365*2)).strftime("%Y-%m-%d")
     data = yahooFinance.download(ticker, start_date, end_date)
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.plot(data.index, data['Close'], color='#43B581')
+    ax.spines['top'].set_color('white')
+    ax.spines['bottom'].set_color('white')
+    ax.spines['left'].set_color('white')
+    ax.spines['right'].set_color('white')
+    ax.tick_params(axis='both', colors='white')
+    ax.set_xlabel('Date', color='white')
+    ax.set_ylabel('Close Price (USD)', color='white')
+    ax.set_title(f'{ticker.upper()} Stock Price', color='white')
+    
+    imageByteArr = io.BytesIO()
+    plt.savefig(imageByteArr, dpi=200, transparent=True)
+    imageByteArr.seek(0)
+    return imageByteArr
 
-    print(data.tail())
 
 class FinanceModule(interactions.Extension):
     def __init__(self,client):
         self.client = client
 
-    # command to get stock values
     @interactions.extension_command(
-        name ="hex_finance_price",
-        description = "Checks the value of a stock (?); to be updated",
+        name ="hex_stock_price",
+        description = "Checks the current value of a stock",
         options = [
             interactions.Option(
-                name = "stock",
-                description = "Stock name",
+                name = "ticker",
+                description = "Stock ticker",
                 type = interactions.OptionType.STRING,
                 required = True
             )
         ]
     )
-    async def finance_price_command(self,ctx: interactions.CommandContext, stock: str):
-        await ctx.send(get_market_price(stock))
+    async def finance_stock_command(self,ctx: interactions.CommandContext, ticker: str):
+        await ctx.send(get_market_price(ticker))
 
+    @interactions.extension_command( 
+        name ="hex_stock_plot",
+        description = "Plot a graph showing the stock closing prices of the last two years.",
+        options = [
+            interactions.Option(
+                name = "ticker",
+                description = "Stock ticker",
+                type = interactions.OptionType.STRING,
+                required = True
+            )
+        ]
+    )
+    async def finance_plot_command(self,ctx: interactions.CommandContext, ticker: str):
+        await command_send(ctx,"",files = interactions.File(fp = get_history(ticker),filename='plot.png'))
 def setup(client):
     FinanceModule(client)
